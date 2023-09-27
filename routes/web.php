fdb54c32bb5ca2045046f9f2b180f930e6f7be17
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-09-27 17:04:32 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
-use App\Http\Controllers\ClassesController;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ClassesController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\DashboardController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,22 +34,25 @@ Route::get('/', function () {
 Route::get('/classes/payment', function () {
     return Inertia::render('Payment');
 })->name('payment');
+
 Route::get('/classes', [ClassesController::class, 'index'])->name('classes.index');
 
 Route::get('/classes/create', [ClassesController::class, 'create'])->name('classes.create');
 Route::get('/classes/{id}', [ClassesController::class, 'show'])->name('classes.show');
+Route::get('/classes/{id}/enroll', [ClassesController::class, 'enroll'])->name('classes.enroll')->middleware('auth');
+
 Route::post('/class/store', [ClassesController::class, 'store'])->name('class.store');
-Route::post('/order/store', [OrderController::class, 'store'])->name('order.store');
-Route::post('/student/payment', [StudentController::class, 'createpayment'])->name('student.payment');
+Route::post('/order/store', [OrdersController::class, 'store'])->name('order.store');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::post('/payment', [PaymentController::class, 'store'])->name('payment');
 
-// Route::middleware('auth')->group(function () {
-Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__ . '/auth.php';
