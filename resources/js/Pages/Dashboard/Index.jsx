b4821dafe75c605cc7dfa38a9794cd63dcd2bbc3
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-09-28 22:12:07 +0800

    added "my classes" to dashboard

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -2,7 +2,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import Payment from "./Partials/Payment";
 
-export default function Dashboard({ auth, orders }) {
+export default function Dashboard({ auth, orders, classes }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -14,26 +14,43 @@ export default function Dashboard({ auth, orders }) {
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
+            <div>
+                <h2>Open Orders</h2>
+                {orders.map((item) =>
+                    item.state === "OPEN" ? (
+                        <div
+                            key={item.id}
+                            className="col-span-2 sm:col-span-1 border"
+                        >
+                            <p>{JSON.parse(item.items_names)}</p>
+                            <p>{item.order_total}</p>
+                            <Payment id={item.id} amount={item.order_total} />
                         </div>
-                    </div>
-                </div>
-
-                {orders.map((item) => (
-                    <div
-                        key={item.id}
-                        className="col-span-2 sm:col-span-1 border"
-                    >
-                        <p>{JSON.parse(item.items_names)}</p>
-                        <p>{item.order_total}</p>
-                        <Payment id={item.id} amount={item.order_total} />
-                    </div>
-                ))}
+                    ) : null
+                )}
             </div>
+
+            <div>
+                <h2>Completed Orders</h2>
+                {orders.map((item) =>
+                    item.state === "COMPLETED" ? (
+                        <div
+                            key={item.id}
+                            className="col-span-2 sm:col-span-1 border"
+                        >
+                            <p>{JSON.parse(item.items_names)}</p>
+                            <p>{item.order_total}</p>
+                        </div>
+                    ) : null
+                )}
+            </div>
+            <h2>My classes</h2>
+            {classes.map((item) => (
+                <div key={item.id} className="col-span-2 sm:col-span-1 border">
+                    <p>{item.name}</p>
+                    <p>{item.description}</p>
+                </div>
+            ))}
         </AuthenticatedLayout>
     );
 }
