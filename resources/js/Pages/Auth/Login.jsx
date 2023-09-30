b5889d299d2bd7263aecc2c2dfe3326b5c0dd1e3
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-09-30 10:51:24 +0800

    styled facebook button

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -99,13 +99,22 @@ export default function Login({ status, canResetPassword }) {
                         Log in
                     </PrimaryButton>
 
-                    <div>
-                        <a href="/login/Facebook" class="btn btn-primary">
-                            Login with Facebook
-                        </a>
-                    </div>
+                    <div></div>
                 </div>
             </form>
+            <div className="flex justify-center items-center gap-2">
+                <a href="/login/Facebook" class="loginBtn loginBtn--facebook">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                    >
+                        <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                    </svg>
+                    <span>Login with Facebook</span>
+                </a>
+            </div>
         </GuestLayout>
     );
 }
