825fb0de0dd91fc8440dabdcbcb971abbee6484c
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-10-09 22:24:41 +0800

    created lib folder and tabs file to hold tabs

---
+++ resources/js/Lib/tabs.jsx
@@ -0,0 +1,50 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faHouse,
+    faCalendarDays,
+    faCartShopping,
+    faGear,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const DashboardTabs = [
+    {
+        label: "Home",
+        icon: (
+            <FontAwesomeIcon
+                icon={faHouse}
+                size="xl"
+                style={{ color: "#5c749d" }}
+            />
+        ),
+    },
+    {
+        label: "My Classes",
+        icon: (
+            <FontAwesomeIcon
+                icon={faCalendarDays}
+                size="xl"
+                style={{ color: "#5c749d" }}
+            />
+        ),
+    },
+    {
+        label: "Orders",
+        icon: (
+            <FontAwesomeIcon
+                icon={faCartShopping}
+                size="xl"
+                style={{ color: "#5c749d" }}
+            />
+        ),
+    },
+    {
+        label: "Settings",
+        icon: (
+            <FontAwesomeIcon
+                icon={faGear}
+                size="xl"
+                style={{ color: "#5c749d" }}
+            />
+        ),
+    },
+];
