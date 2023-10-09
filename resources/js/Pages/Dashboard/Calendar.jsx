10776029da6f45bc81fb1ddb5dc1e25f7fee150a
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-10-09 22:23:26 +0800

    created calendar and orders components

---
+++ resources/js/Pages/Dashboard/Calendar.jsx
@@ -0,0 +1,8 @@
+export default function Calendar({ user, classes }) {
+    console.log(classes);
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    );
+}
--- resources/js/Pages/Dashboard/Dashboard.jsx
+++ resources/js/Pages/Dashboard/Dashboard.jsx
@@ -1,74 +1,32 @@
+import { useState } from "react";
+import { DashboardTabs } from "@/Lib/tabs";
 import Layout from "@/Layouts/Layout";
-import Home from "./Home";
 import { Head } from "@inertiajs/react";
 import TabNav from "@/Components/TabNav";
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faHouse,
-    faCalendarDays,
-    faCartShopping,
-    faGear,
-} from "@fortawesome/free-solid-svg-icons";
+import Home from "./Home";
+import Calendar from "./Calendar";
+import Orders from "./Orders";
 
-export default function Dashboard({ auth, orders, classes }) {
-    const tabs = [
-        {
-            label: "Home",
-            icon: (
-                <FontAwesomeIcon
-                    icon={faHouse}
-                    size="xl"
-                    style={{ color: "#5c749d" }}
-                />
-            ),
-        },
-        {
-            label: "My Classes",
-            icon: (
-                <FontAwesomeIcon
-                    icon={faCalendarDays}
-                    size="xl"
-                    style={{ color: "#5c749d" }}
-                />
-            ),
-        },
-        {
-            label: "Orders",
-            icon: (
-                <FontAwesomeIcon
-                    icon={faCartShopping}
-                    size="xl"
-                    style={{ color: "#5c749d" }}
-                />
-            ),
-        },
-        {
-            label: "Settings",
-            icon: (
-                <FontAwesomeIcon
-                    icon={faGear}
-                    size="xl"
-                    style={{ color: "#5c749d" }}
-                />
-            ),
-        },
-    ];
-
+export default function Dashboard({ auth, student, orders, classes }) {
     const [activeTab, setActiveTab] = useState(0);
     const handleTabChange = (index) => {
         setActiveTab(index);
     };
+    console.log(orders);
     return (
         <Layout user={auth.user}>
             <Head title="Dashboard" />
             <TabNav
-                tabs={tabs}
+                tabs={DashboardTabs}
                 activeTab={activeTab}
                 onTabChange={handleTabChange}
             />
             <div className="sm:pt-24 max-w-7xl mx-auto h-screen">
-                <Home user={auth.user} />
+                {activeTab == 0 && <Home user={auth.user} />}
+                {activeTab == 1 && (
+                    <Calendar user={auth.user} classes={classes} />
+                )}
+                {activeTab == 2 && <Orders user={auth.user} orders={orders} />}
             </div>
         </Layout>
     );
