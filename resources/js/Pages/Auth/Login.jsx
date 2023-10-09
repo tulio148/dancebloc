adc719692f7989a2cb309dbbbbf756b70f77e617
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-10-09 22:25:08 +0800

    added register button to login page

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -145,8 +145,14 @@ export default function Login({ status, canResetPassword }) {
                                     >
                                         Log in
                                     </PrimaryButton>
-
-                                    <div></div>
+                                </div>
+                                <div className="flex items-center justify-end mt-4">
+                                    <Link
+                                        href={route("register")}
+                                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    >
+                                        Not registered yet? Sign up!
+                                    </Link>
                                 </div>
                             </form>
                         )}
