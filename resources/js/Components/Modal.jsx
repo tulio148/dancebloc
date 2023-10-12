2a84ea7a97581a3038771f07ddbebd89ea62afaf
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-10-12 21:42:08 +0800

    changed modal to take className not only maxWidth

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -4,7 +4,7 @@ import { Dialog, Transition } from "@headlessui/react";
 export default function Modal({
     children,
     show = false,
-    maxWidth = "2xl",
+    className = "",
     closeable = true,
     onClose = () => {},
 }) {
@@ -14,22 +14,12 @@ export default function Modal({
         }
     };
 
-    const maxWidthClass = {
-        sm: "sm:max-w-sm",
-        md: "sm:max-w-md",
-        lg: "sm:max-w-lg",
-        xl: "sm:max-w-xl",
-        "2xl": "sm:max-w-2xl",
-        "3xl": "sm:max-w-3xl",
-        "4xl": "sm:max-w-4xl",
-    }[maxWidth];
-
     return (
         <Transition show={show} as={Fragment} leave="duration-200">
             <Dialog
                 as="div"
                 id="modal"
-                className="fixed inset-0 flex overflow-y-auto px-4 py-6 sm:px-0 items-center z-50 transform transition-all"
+                className="fixed inset-0 flex overflow-y-auto  py-6 px-0 items-center z-50 transform transition-all"
                 onClose={close}
             >
                 <Transition.Child
@@ -54,7 +44,7 @@ export default function Modal({
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                     <Dialog.Panel
-                        className={` bg-white rounded-md overflow-hidden transform transition-all h-fit sm:w-full mx-auto ${maxWidthClass}`}
+                        className={` bg-white rounded-md transform transition-all h-fit  mx-auto ${className}`}
                     >
                         {children}
                     </Dialog.Panel>
