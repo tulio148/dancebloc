a85b7705f12ea3258f522a11cacccd1ea73fd71f
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-10-17 20:12:29 +0800

    reworked classCard

--- resources/js/Components/ClassCard.jsx
+++ resources/js/Components/ClassCard.jsx
@@ -1,34 +1,44 @@
 import { Link } from "@inertiajs/react";
 import formatDate from "@/Lib/dateformatter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 
 export default function ClassCard({ class_ }) {
     const date = formatDate(class_.datetime);
 
     return (
-        <Link
-            href={route("classes.show", class_.name)}
-            className="bg-white max-w-3xl flex flex-wrap justify-center overflow-hidden rounded-xl drop-shadow-2xl mx-8  "
+        <div
+            className="flex flex-col gap-4 max-w-lg border-b bg-gradient-to-b from-white/90 from-70% lg:sm:min-w-[700px] sm:min-w-[500px] min-w-[320px] px-6 py-8 rounded-3xl  mx-3 my-10 z-40
+        "
         >
-            <div className="w-full md:w-1/2 ">
-                <img
-                    src="\image.webp"
-                    alt="alt"
-                    className="w-full max-w-md h-auto object-cover"
-                />
+            <p className="text-xl underline decoration-2 decoration-db-green underline-offset-8 text-slate-600">
+                <span className="mr-4">
+                    <FontAwesomeIcon
+                        icon={faCalendar}
+                        size="lg"
+                        style={{ color: "#FF00F7" }}
+                    />
+                </span>
+                <span>{date}</span>
+            </p>
+            <div className="flex flex-wrap items-center gap-3 pt-5">
+                <h2 className="max-w-fit text-xl font-semibold text-db-pink tracking-widest ">
+                    {class_.name}
+                </h2>
+                <span className="max-w-fit text-lg border border-db-green/50 px-4 py-2 tracking-wide rounded-full text-slate-500 ">
+                    {class_.level}
+                </span>
             </div>
 
-            <div className="w-full md:w-1/2 px-4 py-8 flex flex-col justify-between gap-5">
-                <div className="flex justify-between items-center ">
-                    <h2 className="text-lg font-semibold italic">
-                        {class_.name}
-                    </h2>
-                    <p className="text-sm text-gray-500">{class_.level}</p>
-                </div>
-                <p className="text-gray-600 text-sm text-justify leading-5 overflow-ellipsis overflow-hidden ">
-                    {class_.description}
-                </p>
-                <p className="text-md text-gray-500 place-self-end">{date}</p>
-            </div>
-        </Link>
+            <p className=" text-slate-600 text-base text-justify tracking-wider leading-7 whitespace-normal py-5">
+                {class_.description}
+            </p>
+            <Link
+                href={route("classes.show", class_.name)}
+                className="self-center w-fit bg-db-green/90 py-4 px-12 mt-2 rounded-xl text-slate-50 tracking-widest text-2xl font-semibold hover:bg-db-green/700 drop-shadow-xl "
+            >
+                Learn More
+            </Link>
+        </div>
     );
 }
