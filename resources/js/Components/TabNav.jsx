654f6149dc806dc8d7149ec2eb273013298da715
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-10-25 10:44:20 +0800

    created func to bring scroll position to top on tab change on tabnav

--- resources/js/Components/TabNav.jsx
+++ resources/js/Components/TabNav.jsx
@@ -1,18 +1,24 @@
 export default function TabNav({ tabs, activeTab, onTabChange, className }) {
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    };
     return (
         <div
-            className={`fixed  bottom-0 sm:top-20 sm:pt-10 w-full h-fit bg-white sm:bg-db-pink ${className}`}
+            className={`fixed bottom-0 sm:top-20 sm:pt-10 w-full h-fit bg-white sm:bg-db-pink border-t border-db-pink ${className}`}
         >
             <div className="flex  gap-2 p-5 sm:px-6 sm:p-0 w-full max-w-7xl mx-auto">
                 {tabs.map((tab, index) => (
                     <button
                         key={index}
-                        onClick={() => onTabChange(index)}
+                        onClick={() => {
+                            onTabChange(index);
+                            scrollToTop();
+                        }}
                         className={`w-full mx-auto py-3 relative z-10 rounded-tr-md rounded-tl-md sm:bg-gradient-to-b   
                 ${
                     index === activeTab
-                        ? "transition-colors duration-700 text-white font-medium from-slate-200/90 from-2%"
-                        : "text-slate-800 text-opacity-90 transition-colors duration-300 from-slate-200/50 hover:from-slate-200/70"
+                        ? "transition duration-700 text-white font-medium from-slate-200/80"
+                        : "text-slate-800 text-opacity-90 transition duration-700 from-slate-200/50 hover:from-slate-200/60"
                 }`}
                     >
                         <span className="inline ">{tab.icon}</span>
@@ -20,7 +26,7 @@ export default function TabNav({ tabs, activeTab, onTabChange, className }) {
                             {tab.label}
                         </span>
                         <div
-                            className={`absolute bottom-0 left-0 w-full border-t border-db-pink/30 sm:border-slate-200/60 transition-transform duration-500 transform origin-left ${
+                            className={`absolute bottom-0 left-0 w-full border-t border-db-pink/30 sm:border-slate-200/40 transition-transform duration-1000 transform origin-left ${
                                 index === activeTab
                                     ? "scale-x-100"
                                     : "scale-x-0"
