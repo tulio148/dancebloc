200363a2f92f799ee641bf4b0181af0a21e1c01a
Tulio Gerbassi <tuliogerbassi@gmail.com>
2024-03-11 21:00:19 +0800

    updated logo on navbar

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -39,7 +39,7 @@ export default function ({ user, children }) {
                         <div className="flex justify-between items-center w-full">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationLogo />
                                 </Link>
                             </div>
                             <Nav user={user} />
